0b84800d2cdcb9b96f7b37a144bdfaab789af3a5
Navil Rodrigues <32533176+rodriguesnavil@users.noreply.github.com>
2017-10-30 12:41:29 +0530

    Commented Feedback.php
    Added comment for each steps

--- html/feedback.php
+++ html/feedback.php
@@ -1,21 +1,25 @@
 <?php
-
+  // Getting the required files
   require 'dbh.php';
-
+// Removing escape character to prevent sql injection
 $fname=mysqli_real_escape_string($conn,$_POST['first_name']);
 $lname=mysqli_real_escape_string($conn,$_POST['last_name']);
 $email=mysqli_real_escape_string($conn,$_POST['email']);
 $message=mysqli_real_escape_string($conn,$_POST['message']);
+// Check if any parameters are not empty 
 if (empty($fname) ||empty($lname) || empty($email) || empty($message)) {
   echo "values not input";
 }
 else {
+  //Creating insert queru
   $sql= "INSERT into feedback(Firstname,Lastname,Email,Message) values ('$fname','$lname','$email','$message');";
+  //Execute the query
   mysqli_query($conn,$sql);
   echo " successs";
+  //Exit after data is inserted
   exit();
 }
-
+//phpmail is function used to send mail to respective users
 phpmail($email,"feedback Successfull",THANK YOU FOR YOU FEEDbACK WE WILL GET WITH YOU,rathilvasani@gmail.com);
 
 ?>
